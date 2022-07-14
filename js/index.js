4689f865a2bf2f617e53f599895d0f7873dcd2c6
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-07-14 15:39:12 +0100

    finished all the sections, started adding responssiveness

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+//side nav display
+//display button after first page
+const sideNavContainer = document.querySelector(".side-nav-container");
+const sideNavDiv = document.querySelector(".side-nav-div");
+const sideNavMenu = document.querySelector(".side-nav-menu");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        sideNavMenu.style.display = "block";
+    } else {
+        sideNavMenu.style.display = "none";
+    }
+}
+
+//display nav on click
+let showSideNav = () => {
+    if (sideNavDiv.classList.contains("d-none")) {
+        sideNavDiv.classList.remove("d-none");
+        sideNavDiv.classList.add("side-nav-div-animateIn");
+        sideNavDiv.classList.add("d-flex");
+    } else {
+        sideNavDiv.classList.add("d-none");
+        sideNavDiv.classList.remove("d-flex");
+    }
+};
+
+sideNavMenu.addEventListener("click", showSideNav);
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach(section => {
+    section.addEventListener("click", function () {
+        sideNavDiv.classList.add("d-none");
+        sideNavDiv.classList.remove("d-flex");
+    });
+});
+
 //drop shadow on navbar when scrolling
 let showBoxShadow = () => {
     const nav = document.querySelector("nav")
